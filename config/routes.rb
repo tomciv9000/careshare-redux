96d2f822d80d3a070837032f1af143b0ea1364c7
tomciv9000 <eugene.white@gmail.com>
2020-09-04 15:23:07 -0400

    Build Sleep resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sleeps
   resources :foods
   resources :diapers
   resources :children
---
+++ db/migrate/20200904192255_create_sleeps.rb
@@ -0,0 +1,13 @@
+class CreateSleeps < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sleeps do |t|
+      t.boolean :nap
+      t.boolean :bedtime
+      t.time :start_time
+      t.time :end_time
+      t.int :shift_id
+
+      t.timestamps
+    end
+  end
+end
