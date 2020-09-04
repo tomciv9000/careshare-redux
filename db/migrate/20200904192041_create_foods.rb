4432d5c02a515d2b80aaa98e55f0b93ae9761079
tomciv9000 <eugene.white@gmail.com>
2020-09-04 15:20:48 -0400

    Build Food resource

---
+++ db/migrate/20200904192041_create_foods.rb
@@ -0,0 +1,12 @@
+class CreateFoods < ActiveRecord::Migration[6.0]
+  def change
+    create_table :foods do |t|
+      t.string :meal_type
+      t.string :description
+      t.time :time
+      t.int :shift_id
+
+      t.timestamps
+    end
+  end
+end
