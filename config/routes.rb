2cdb24a1673ab6f9cbce93076f5986d7a184e25f
tomciv9000 <eugene.white@gmail.com>
2020-09-18 19:48:31 -0400

    Scope routes to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  resources :notes
-  resources :sleeps
-  resources :foods
-  resources :diapers
-  resources :children
-  resources :users
+  scope '/api' do
+    post 'user_token' => 'user_token#create'
+    resources :notes
+    resources :sleeps
+    resources :foods
+    resources :diapers
+    resources :children
+    resources :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
+Rails.application.routes.draw do
+  scope '/api' do
+    post 'user_token' => 'user_token#create'
+    post '/users' => 'users#create'
+  end
 end
