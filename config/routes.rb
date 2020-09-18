01495ad8b518384743df80111282a33a88be8730
tomciv9000 <eugene.white@gmail.com>
2020-09-18 19:53:02 -0400

    Add knock to application controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,3 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-
-Rails.application.routes.draw do
-  scope '/api' do
-    post 'user_token' => 'user_token#create'
-    post '/users' => 'users#create'
-  end
-end
