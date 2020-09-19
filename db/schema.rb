7800ec70dc9610843369203de301c11de9c0c02f
tomciv9000 <eugene.white@gmail.com>
2020-09-19 15:03:10 -0400

    Migrate shift to db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_033937) do
+ActiveRecord::Schema.define(version: 2020_09_19_180953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2020_09_19_033937) do
   create_table "notes", force: :cascade do |t|
     t.string "content"
     t.integer "shift_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
