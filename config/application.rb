3674c0d85ed72e68d0fe3f3a48ce1e242fcf1b7d
tomciv9000 <eugene.white@gmail.com>
2020-09-19 11:55:08 -0400

    at rack-cors to config

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,15 @@ module CareshareRedux
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*' 
+        resource '*',
+        headers: :any,
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        methods: [:get, :post, :options, :delete, :put, :patch]
+      end
+    end
     config.api_only = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-
-  scope '/api' do
-    
-    resources :notes
-    resources :sleeps
-    resources :foods
-    resources :diapers
-    resources :children
-    
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
