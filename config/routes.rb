7a3eb2e1b9af181a767cdb55041d54abb4607f1c
tomciv9000 <eugene.white@gmail.com>
2020-09-18 23:39:54 -0400

    Devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+
   scope '/api' do
-    post 'user_token' => 'user_token#create'
+    
     resources :notes
     resources :sleeps
     resources :foods
     resources :diapers
     resources :children
-    resources :users
+    
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
