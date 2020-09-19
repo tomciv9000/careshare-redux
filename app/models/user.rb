1d5ac2a0d2414b915af1c115a5023b0f0612dcfa
tomciv9000 <eugene.white@gmail.com>
2020-09-18 22:30:59 -0400

    Update user model with devise auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+    extend Devise::Models #added this line to extend devise model
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
+    include DeviseTokenAuth::Concerns::User
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   scope '/api' do
     post 'user_token' => 'user_token#create'
     resources :notes
