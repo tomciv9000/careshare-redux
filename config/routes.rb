8c7970f07adf20f692c1286f19cfd9faf35cb749
tomciv9000 <eugene.white@gmail.com>
2020-09-19 15:13:16 -0400

    Add model resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :shifts
   namespace :api do
     scope :v1 do
+      resources :food
+      resources :children
+      resources :shifts
+      resources :diapers
+      resources :notes
+      resources :sleep
+
       mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
