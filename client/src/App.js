f4bd25dd6dea052aee4a0b9b0f8a6cb73e68ccec
tomciv9000 <eugene.white@gmail.com>
2020-09-19 18:51:03 -0400

    Initial welcome page skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navbar bg="light" expand="large">
+        <Navbar.Brand>CareShare</Navbar.Brand>
+      </Navbar>
+      <Container fluid>
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  </div> 
+    
   );
 }
 
