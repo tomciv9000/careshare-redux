e6d8dd15d67bf5939acdb62efd55684af3a40872
tomciv9000 <eugene.white@gmail.com>
2020-09-20 12:35:54 -0400

    Add Landing Page and React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+
 
 function App() {
   return (
