eaea767e2d84b59302d03b5ee734d394d7672c13
tomciv9000 <eugene.white@gmail.com>
2020-09-20 13:14:55 -0400

    Add redux toolkit

---
+++ client/src/reducers/usersReducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+    isAuthenticated: false,
+    currentUser: {},
+    errors:''
+  };
+  
+export default function usersReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'LOGIN_USER':
+      return {...state, 
+        isAuthenticated: !!(Object.keys(action.payload).length),
+        currentUser: action.payload,
+        errors: ''
+  
+      }
+    case 'LOGOUT_USER':
+      return {...state, 
+        isAuthenticated: false,
+        currentUser: {},
+        errors: '' }
+    case 'LOGIN_FAIL':
+      return {...state, 
+        isAuthenticated: false,
+        currentUser: {},
+        errors: action.payload
+      }
+    default:
+      return state;
+    }
+}
