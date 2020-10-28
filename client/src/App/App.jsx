8eebb9fc221bdcd3cd6fedb1bf84c5b7aa812c4b
tomciv9000 <eugene.white@gmail.com>
2020-10-27 20:46:25 -0400

    Clean up unused variables

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
-//import { connect } from 'react-redux';
+
 import { useDispatch, useSelector } from 'react-redux'
-//import { useHistory, useLocation } from 'react-router-dom';
+
 import { history } from '../_helpers/history';
 import { alertActions } from '../_actions/alert.actions';
 import { PrivateRoute } from '../_components/PrivateRoute';
 import { HomePage } from '../HomePage/HomePage';
 import { LoginPage } from '../LoginPage/LoginPage';
 import { RegisterPage } from '../RegisterPage/RegisterPage';
-import { Container, Jumbotron, Row, Col, Alert} from 'react-bootstrap'
+import { Container, Jumbotron, Alert} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App = () => {
 
     const dispatch = useDispatch()
     const alert = useSelector(state => state.alert)
-    console.log(alert)
-    const clearAlerts = dispatch(alertActions.clear)
+    
     
     history.listen((location, action) => {
      // clear alert on location change
