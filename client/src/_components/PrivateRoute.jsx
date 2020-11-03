17fb771b5fb2ea4aa74c0e984c95d5eda0c0c65d
tomciv9000 <eugene.white@gmail.com>
2020-11-03 11:44:18 -0500

    Comment out PrivateRoute

--- client/src/_components/PrivateRoute.jsx
+++ client/src/_components/PrivateRoute.jsx
@@ -3,20 +3,20 @@ import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
+// export const PrivateRoute = ({ component: Component, ...rest }) => (
     
-    <Route {...rest} render={props => (
-        localStorage.getItem('user')
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )} />
-)
-export const AuthRouteX = props => {
+//     <Route {...rest} render={props => (
+//         localStorage.getItem('user')
+//             ? <Component {...props} />
+//             : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+//     )} />
+// )
+export const PrivateRoute = props => {
     const isAuthenticated = useSelector(state => state.authentication.loggedIn) 
     const { type } = props;
     
-    if (type === "guest" && isAuthenticated) return <Redirect to="/" />;
-    else if (type === "private" && !isAuthenticated) return <Redirect to="/" />;
+    if (type === "guest" && isAuthenticated) return <Redirect to={{ pathname: '/', state: { from: props.location } }} />;
+    else if (type === "private" && !isAuthenticated) return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />;
   
     return <Route {...props} />;
   };
