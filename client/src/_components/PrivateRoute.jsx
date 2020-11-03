3803a9d756e79f7e765a55356f4ad4465392417a
tomciv9000 <eugene.white@gmail.com>
2020-11-03 11:19:37 -0500

    Import useSelector

--- client/src/_components/PrivateRoute.jsx
+++ client/src/_components/PrivateRoute.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
+    
     <Route {...rest} render={props => (
         localStorage.getItem('user')
             ? <Component {...props} />
