6a6c1a8e401cb8fc419722a690a9bea02bdeb0da
tomciv9000 <eugene.white@gmail.com>
2020-11-07 23:32:38 -0500

    Remove user list in homepage

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -19,23 +19,7 @@ class HomePage extends React.Component {
             <div className="col-md-6 col-md-offset-3">
                 <h1>Hi guy</h1>
                 <p>You're logged in with React!!</p>
-                {/* <h3>All registered users:</h3> */}
-                {/* {users.loading && <em>Loading users...</em>} */}
-                {/* {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
-                    <ul>
-                        {users.items.map((user, index) =>
-                            <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                                {
-                                    user.deleting ? <em> - Deleting...</em>
-                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
-                                }
-                            </li>
-                        )}
-                    </ul>
-                } */}
+                
                 <p>
                     <Link to="/login">Logout</Link>
                 </p>
