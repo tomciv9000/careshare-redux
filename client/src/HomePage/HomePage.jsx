686adccde0f8d924033cfa238cde12152b5fcf91
tomciv9000 <eugene.white@gmail.com>
2020-11-09 13:19:45 -0500

    Convert Homepage to functional component

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { userActions } from '../_actions/user.actions';
+import Button from 'react-bootstrap/Button'
 //BOOKMARK - CONVET THIS TO A FUNCTIONAL COMPONENT AND ADD LOGOUT
 
 export const HomePage = () => {
+    const dispatch = useDispatch()
+    const logout = userActions.logout
+
+    const activeUser = useSelector(state => state.authentication.user)
+
+    const handleClick = (event) => {
+        console.log("Homepage Logout clicked")
+        event.preventDefault()
+        dispatch(logout())
+      }
+
+    
     return (
         <div>
             <h1>C a r e S h a r e</h1>
             <h3>User Profile Page (Private)</h3>
-            <p>You're logged in with React!!</p>
+            <p>Hello, {activeUser.uid}!</p>
+            <p>
+            <Button onClick={handleClick}variant="outline-primary" size="sm">Log Out</Button>
+            </p>
         </div>
     )
 }
