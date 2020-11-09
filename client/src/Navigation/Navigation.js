6664e58d0ef6ea3c54983d75264e27b7955f1de5
tomciv9000 <eugene.white@gmail.com>
2020-11-09 13:23:36 -0500

    Refactor NavBar

--- client/src/Navigation/Navigation.js
+++ client/src/Navigation/Navigation.js
@@ -20,7 +20,6 @@ export const NavBar = () => {
     dispatch(logout())
   }
 
-  console.log("activeuser", activeUser)
 
   const userLinks = (
     <Nav>
@@ -56,63 +55,3 @@ export const NavBar = () => {
       </Navbar>
   )
 }
-
-
-/* class NavigationBar extends Component {
-
-  handleClick = event => {
-    console.log("clicked")
-    event.preventDefault()
-    this.props.signOutRequest()
-  }
-
-  render() {
-    const userLinks = (
-      <Nav>
-        <Navbar.Text>
-          Account: {this.props.currentUser.email}
-        </Navbar.Text>
-        <LinkContainer to="/">
-          <Nav.Link onClick={this.handleClick}>Log Out</Nav.Link>
-        </LinkContainer>
-      </Nav>
-    )
-
-    const guestLinks = (
-      <Nav>
-        <LinkContainer to="/signup">
-          <Nav.Link>Create Account</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/login">
-          <Nav.Link>Sign in</Nav.Link>
-        </LinkContainer>
-      </Nav>
-    )
-
-    return (
-      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-        <Navbar.Brand>
-          CareShare
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          {this.props.isAuthenticated ? userLinks : guestLinks}
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.user.isAuthenticated,
-    currentUser: state.user.currentUser
-  };
-}
-
-const mapDispatchToProps = dispatch => ({
-    signOutRequest: () => dispatch(signOutRequest())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(NavigationBar);
- */
