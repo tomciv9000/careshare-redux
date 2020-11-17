a6d79efdaa030cdb10e2789e4a300622224f271d
tomciv9000 <eugene.white@gmail.com>
2020-11-17 14:55:21 -0500

    Remove useDispatch import on NewChildForm

--- client/src/NewChildForm/NewChildForm.jsx
+++ client/src/NewChildForm/NewChildForm.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useDispatch } from "react-redux"
+//åimport { useDispatch } from "react-redux"
 import { Link } from "react-router-dom"
 import { Formik } from "formik"
 import * as Yup from "yup"
